1cf2fabd0e8d94c561cbd963d086e70b53c9730e
TaHuC <TaHuC@hotmail.com>
2017-01-25 08:50:18 +0200

    Finish

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Greeting
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var firstName = Console.ReadLine();
+			var lastName = Console.ReadLine();
+			int age = int.Parse(Console.ReadLine());
+
+			Console.WriteLine($"Hello, {firstName} {lastName}. You are {age} years old.");
+		}
+	}
+}
